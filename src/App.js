cc315d083f2502207ee8d0d2190a169c62f2962e
RuichenHe <kylehe970128@gmail.com>
2024-04-02 19:21:57 -0500

    conf signin

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import './App.css';
 import './common/style.css'
 import { Link, Outlet } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [userInfo, setUserInfo] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setUserInfo(await getUserInfo());
+    })();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
+  }
 
   return (
     <div className="App">
       <div className="top-bar">
         <div className='home-head'>
-          <Link to="/todos">Ruichen's TODO List</Link>
+        {userInfo && <Link to="/todos">{userInfo.userDetails}'s TODO List</Link>}
         </div>
         <div className='bottom-buttons'>
           <div className='bottom-button-container'>
-            <Link to="/todos" className='bottom-link-todo'>TODO List</Link>
+          {userInfo && <Link to="/todos" className='bottom-link-todo'>TODO List</Link>}
           </div>
           <div className='bottom-button-container'>
-            <Link to="/done" className='bottom-link-done'>DONE List</Link>
+          {userInfo && <Link to="/done" className='bottom-link-done'>DONE List</Link>}
+          </div>
+          <div className='bottom-button-container'>
+          {userInfo && <a href='/.auth/logout' className='bottom-link-done'>Log out</a>}
           </div>
         </div>
       </div>
-      <a href="/.auth/login/github?post_login_redirect_uri=/todos">Login</a>
-      <a href='/.auth/logout'>Log out</a>
+      {!userInfo && <a href="/.auth/login/github?post_login_redirect_uri=/todos">Login</a>}
+      
       <Outlet />
     </div>
   );
