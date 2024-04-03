8a7f466fb36d8d7dea7502a42c7889d9bb5d8599
RuichenHe <kylehe970128@gmail.com>
2024-04-02 20:43:46 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       const response = await fetch('/.auth/me');
       const payload = await response.json();
       const { clientPrincipal } = payload;
-      console.log(clientPrincipal)
+      //console.log(clientPrincipal)
       return clientPrincipal;
     } catch (error) {
       console.error('No profile could be found');
