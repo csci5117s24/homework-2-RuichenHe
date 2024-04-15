b5c85709b7373090cab983c74c9d22a8e0ab6291
RuichenHe <kylehe970128@gmail.com>
2024-04-15 13:34:49 -0500

    Add style to NotFOund page

--- src/routes/Not_found.jsx
+++ src/routes/Not_found.jsx
@@ -1,7 +1,13 @@
 import {Link} from 'react-router-dom'
 export default function NotFound(){
   return (
-    <div>
+    <div className="App color4-back">
+      <div className="top-bar color6-back">
+        <div className='home-head color1-back'>
+        </div>
+        <div className='bottom-buttons'>
+        </div>
+      </div>
       <h1>
         You seem to be lost...
       </h1>
